4f3a5e86bc203b12c36fb43c5d0b7695968bd020
Vadim <uvolkov@374@gmail.com>
2022-07-04 13:31:18 +0300

    Add TASK1

---
+++ DZGIT03/TASK01/Program.cs
@@ -0,0 +1,29 @@
+﻿int n1 = InputInt("Введите число: ");
+
+int n2 = 0;
+int digit = 0;
+int a = n1;
+int count = 0;
+
+ while(count < 5)
+{
+    digit = n1 % 10;
+    n1 = n1 / 10;
+    n2 = (n2 * 10) + digit;
+    count++;
+}
+
+if(a == n2)
+{
+    Console.WriteLine("Palendrom");
+}
+else
+{
+    Console.WriteLine("N0 Palendrom");
+}
+
+int InputInt(string output)
+{
+    Console.WriteLine(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
