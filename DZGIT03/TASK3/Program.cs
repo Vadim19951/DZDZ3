3ec53494f603632fa63ea87ba55335b65965acd2
Vadim <uvolkov@374@gmail.com>
2022-07-04 13:40:22 +0300

    Add Task3

---
+++ DZGIT03/TASK3/Program.cs
@@ -0,0 +1,23 @@
+﻿int n = InputInt("Enter number N in the Sonsole: ");
+
+int number = 1;
+
+int cube = 0;
+
+while(number <= n)
+{
+cube =((number * number) * number);
+Console.WriteLine(cube);
+number++;
+}
+
+
+
+
+int InputInt(string output)
+{
+   Console.WriteLine(output);
+   return Convert.ToInt32(Console.ReadLine());
+}
+
+
